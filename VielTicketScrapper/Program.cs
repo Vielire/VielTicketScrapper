62114d304076d6b808c4511313a71a9fc6a9f50e
Janikowski, Piotr <pjanikowski@kpmg.pl>
2021-03-01 21:00:14 +0100

    - Additional custom Console.Out.WriteLine based on CoutCustom delegate - Simple exception catcher for Intercity static function

--- VielTicketScrapper/Program.cs
+++ VielTicketScrapper/Program.cs
@@ -15,32 +15,36 @@ namespace VielTicketScrapper
 {
     public static class Program
     {
-        static readonly Action<string> cout = (message) => Console.Out.WriteLine(message);
-
-        delegate void VerboseCout(string message, ConsoleColor foregroundColor = ConsoleColor.DarkGray, ConsoleColor bgColor = ConsoleColor.Black);
-        static readonly VerboseCout coutVerbose = (message, foregroundColor, bgColor) =>
+        delegate void CoutCustom(string message, ConsoleColor foregroundColor = ConsoleColor.DarkGray, ConsoleColor bgColor = ConsoleColor.Black);
+        static readonly CoutCustom coutCustom = (message, foregroundColor, bgColor) =>
         {
             Console.BackgroundColor = bgColor;
             Console.ForegroundColor = foregroundColor;
             Console.Out.WriteLine(message);
             Console.ResetColor();
         };
+
+        static readonly Action<string> cout = (message) => Console.Out.WriteLine(message);
+        static readonly Action<string> coutError = (message) => coutCustom(message, ConsoleColor.Red);
+        static readonly Action<string> coutVerbose = (message) => coutCustom(message, ConsoleColor.DarkGray);
+        static readonly Action<string> coutSuccess = (message) => coutCustom(message, ConsoleColor.DarkGreen);
+
         public static async Task<int> Main(string[] args)
         {
-            //var cmd = new RootCommand
-            //{
-            //    new Command("intercity", "Scrap the data from Intercity ticket!")
-            //    {
-            //        new Argument<string>("filepath"),
-            //        new Option<ExportFileType>("--to", "ical/text"),
-            //        new Option<bool>("--verbose")
-            //    }.WithHandler(nameof(Intercity))
-            //};
+            var cmd = new RootCommand
+            {
+                new Command("intercity", "Scrap the data from Intercity ticket!")
+                {
+                    new Argument<string>("filepath"),
+                    new Option<ExportFileType>("--to", "ical/text"),
+                    new Option<bool>("--verbose")
+                }.WithHandler(nameof(Intercity))
+            };
 
-            //return await cmd.InvokeAsync(args);
+            return await cmd.InvokeAsync(args);
 
-            Intercity(@"C:\NO_BACKUP_FILES\Bilety\Intercity_template.pdf", ExportFileType.ICal, false, null);
-            return 0;
+            //Intercity(@"C:\NO_BACKUP_FILES\Bilety\Intercity_template_wymiana.pdf", ExportFileType.ICal, false, null);
+            //return 0;
         }
         private static void Intercity(string filePath, ExportFileType to, bool verbose, IConsole console)
         {
@@ -51,25 +55,37 @@ namespace VielTicketScrapper
             if (verbose)
                 coutVerbose($"About to scrap data from the '{fileName}' file...");
 
-
-            IntercityScrapper scrapper = new IntercityScrapper();
-            TicketModel ticket = scrapper.ScrapPDF(filePath).ParseToTicket();
-            ICal ical = new(ticket);
-
-            switch (to)
+            try
             {
-                case ExportFileType.ICal:
-                    File.WriteAllText(folderPath + DateTime.Now.ToString("yyyyMMddhhmmss_") + ticket.TicketNumber + ".ics", ical.ToString());
-                    cout("You should find iCal file next to the ticket file");
-                    break;
-                default:
-                    File.WriteAllText(folderPath + DateTime.Now.ToString("yyyyMMddhhmmss_") + ticket.TicketNumber + ".txt", ical.ToString());
-                    cout("You should find text file next to the ticket file");
-                    break;
+                IntercityScrapper scrapper = new IntercityScrapper();
+                TicketModel ticket = scrapper.ScrapPDF(filePath).ParseToTicket();
+
+                ICal ical = new(ticket);
+
+                switch (to)
+                {
+                    case ExportFileType.ICal:
+                        File.WriteAllText(folderPath + DateTime.Now.ToString("yyyyMMddhhmmss_") + ticket.TicketNumber + ".ics", ical.ToString());
+                        cout("You should find iCal file next to the ticket file");
+                        break;
+                    default:
+                        File.WriteAllText(folderPath + DateTime.Now.ToString("yyyyMMddhhmmss_") + ticket.TicketNumber + ".txt", ical.ToString());
+                        cout("You should find text file next to the ticket file");
+                        break;
+                }
+
+                coutSuccess($"All done!");
             }
-            
-            if (verbose)
-                coutVerbose($"All done!", ConsoleColor.DarkGreen);
+            catch(NotSupportedException e)
+            {
+                coutError(e.Message);
+                Environment.Exit(1006); // ERROR_FILE_INVALID
+            }
+            catch(Exception e)
+            {
+                coutError(e.Message);
+                Environment.Exit(13); //ERROR_INVALID_DATA
+            }
         }
 
         private static Command WithHandler(this Command command, string methodName)
