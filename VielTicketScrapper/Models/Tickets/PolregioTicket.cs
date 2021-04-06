215e9692c38349c38ae655a17921277e6d88beb3
Piotr Janikowski <janikowskipiotr@outlook.com>
2021-04-06 23:31:24 +0200

    PolregioTicket model added

---
+++ VielTicketScrapper/Models/Tickets/PolregioTicket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VielTicketScrapper.Models.Tickets
+{
+    public class PolregioTicket
+    {
+        public DateTime ValidDateStart { get; set; }
+        public int ValidDurationInHours { get; set; }
+    }
+}
